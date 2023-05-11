00098d56549cf38ab6b74afd2b0523a3d8c578b4
Larisa76 <118682544+Larisa76@users.noreply.github.com>
2023-05-11 22:41:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,35 @@ btn.addEventListener('click', () => {
 })
 
 
-const applicantForm = document.getElementById('form')
-applicantForm.addEventListener('submit', handleFormSubmit)
+// const applicantForm = document.getElementById('form')
+// applicantForm.addEventListener('submit', handleFormSubmit)
 
 
-const isOn = someCheckboxInput.checked
+// const isOn = someCheckboxInput.checked
 
 
-function serializeForm(formNode) {
-  const { elements } = formNode
+// function serializeForm(formNode) {
+//   const { elements } = formNode
 
-  const data = Array.from(elements)
-    .map((element) => {
-      const { name, type } = element
-      const value = type === 'checkbox' ? element.checked : element.value
+//   const data = Array.from(elements)
+//     .map((element) => {
+//       const { name, type } = element
+//       const value = type === 'checkbox' ? element.checked : element.value
 
-      return { name, value }
-    })
-    .filter((item) => !!item.name)
+//       return { name, value }
+//     })
+//     .filter((item) => !!item.name)
   
-  console.log(data)
-}
+//   console.log(data)
+// }
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+fetch(url).then(data => {
+return data.json();
+}).then(data => {
+  console(log)(data)
+})
 
 
 
